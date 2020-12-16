5e0c7c06ca0cd6ffeeaa54fab5be5ca52d4733a1
andrewrreed <areed1242@gmail.com>
2020-12-16 16:33:20 -0500

    use bootstrap

--- script1.py
+++ script1.py
@@ -3,4 +3,36 @@
 
 import os
 
+run_time_suffix = datetime.datetime.now()
+run_time_suffix = run_time_suffix.strftime("%d%m%Y%H%M%S")
+
+# Set the setup variables needed by CMLBootstrap
+HOST = os.getenv("CDSW_API_URL").split(
+    ":")[0] + "://" + os.getenv("CDSW_DOMAIN")
+USERNAME = os.getenv("CDSW_PROJECT_URL").split(
+    "/")[6]  # args.username  # "vdibia"
+API_KEY = os.getenv("CDSW_API_KEY") 
+PROJECT_NAME = os.getenv("CDSW_PROJECT")  
+
+# Instantiate API Wrapper
+cml = CMLBootstrap(HOST, USERNAME, API_KEY, PROJECT_NAME)
+
+# Set the STORAGE environment variable
+try : 
+  storage=os.environ["STORAGE"]
+except:
+  if os.path.exists("/etc/hadoop/conf/hive-site.xml"):
+    tree = ET.parse('/etc/hadoop/conf/hive-site.xml')
+    root = tree.getroot()
+    for prop in root.findall('property'):
+      if prop.find('name').text == "hive.metastore.warehouse.dir":
+        storage = prop.find('value').text.split("/")[0] + "//" + prop.find('value').text.split("/")[2]
+  else:
+    storage = "/user/" + os.getenv("HADOOP_USER_NAME")
+  storage_environment_params = {"STORAGE":storage}
+  storage_environment = cml.create_environment_variable(storage_environment_params)
+  os.environ["STORAGE"] = storage
+
+
 print("DATA_LOCATION:", os.environ['DATA_LOCATION'])
+print("STORAGE", os.environ["STORAGE"])
--- script2.py
+++ script2.py
@@ -1,3 +1,4 @@
 import os
 
 print("DATA_LOCATION:", os.environ['DATA_LOCATION'])
+print("STORAGE", os.environ["STORAGE"])
