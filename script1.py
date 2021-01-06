1da5c2917852c5a4d65d6766df4fc112c0eb5a84
andrewrreed <areed1242@gmail.com>
2021-01-06 13:44:01 -0500

    dfadsf

--- script1.py
+++ script1.py
@@ -32,3 +32,8 @@ try:
     print(os.environ['DATA_LOCATION'])
 except:
     print('DATA_LOCATION env variable is NOT present')
+
+try:
+    print(os.environ['TEST_VAR_1'])
+except:
+    print('TEST_VAR_1 env variable is NOT present')
